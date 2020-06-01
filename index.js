558590f8d55c12a9fa793695831340a0fe9f2925
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-01 17:02:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(array, fn){
+  const transform = [];
+  array.forEach(item => {
+    transform.push(fn(item));
+  });
+  return transform;
+}
+
+function reduce(array, fn, startingPoint){
+  let startingIndex = 0;
+  if (startingPoint == undefined){
+    startingIndex = 1;
+    startingPoint = array[0];
+  }
+
+  let memo = startingPoint;
+  for(let i = startingIndex; i < array.length; i++){
+    memo = fn(array[i], memo);
+  }
+  return memo;
+}
